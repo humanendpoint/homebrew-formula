3321476411aaa7c0509ccd576fa7a1809cfb7479
Tom <104126589+humanendpoint@users.noreply.github.com>
2023-03-30 18:07:59 +0200

    Create brew-pkg.rb

---
+++ brew-pkg.rb
@@ -0,0 +1,14 @@
+class BrewPkg < Formula
+  desc "Homebrew command for building OS X packages from installed formulae"
+  homepage "https://github.com/humanendpoint/brew-pkg"
+  url "https://github.com/humanendpoint/brew-pkg.git", :tag => "v0.1.7",
+                                                   :revision => "157dbe8b4696db890517ec3b6bf0c9aa02aedd82"
+
+  # This is an .rb that must be executable in order for Homebrew to
+  # find it with the 'which' method, so we skip_clean
+  skip_clean "bin"
+
+  def install
+    bin.install "brew-pkg.rb"
+  end
+end
