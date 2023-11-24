35e4896002e4d07e07b62d0e1b72f6d4c0ee85d5
Tom <104126589+humanendpoint@users.noreply.github.com>
2023-11-24 13:49:45 +0100

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,7 +1,7 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae"
   homepage "https://github.com/humanendpoint/brew-pkg"
-  url "https://github.com/humanendpoint/brew-pkg.git", :tag => "v0.1.8",
+  url "https://github.com/humanendpoint/brew-pkg.git", :tag => "v0.1.9",
                                                    :revision => "c824313e530463015f4fc383c1dbae25b3657b97"
 
   # This is an .rb that must be executable in order for Homebrew to
